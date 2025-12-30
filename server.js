126c0714723d31bd49b6e815743522928daef226
Beyondthell <67501329+Beyondthell@users.noreply.github.com>
2025-12-30 17:15:21 +0530

    Remove unused import for sqlite

--- server.js
+++ server.js
@@ -2,8 +2,8 @@ const express = require('express');
 const cors = require('cors');
 const bodyParser = require('body-parser');
 const sqlite3 = require('sqlite3').verbose();
-const { open } = require('sqlite');
 const nodemailer = require('nodemailer');
+
 
 // CONFIG
 const PORT = process.env.PORT || 4000;
